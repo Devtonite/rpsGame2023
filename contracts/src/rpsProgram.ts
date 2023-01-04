89ddb0260d24213f048735898acdc88418abfa47
Devtonite <devtonite001@gmail.com>
2023-01-04 05:29:35 -0600

    Redo zkProgram methods for recursion 1

--- contracts/src/rpsProgram.ts
+++ contracts/src/rpsProgram.ts
@@ -1,76 +1,94 @@
 /* eslint-disable */
 import {
+  Circuit,
   Experimental,
   Field,
   Poseidon,
   SelfProof,
   Struct,
-  UInt32,
 } from 'snarkyjs';
 
 export class rpsState extends Struct({
   player1Choice: Field, // secret fields
-  player2Choice: Field, // secret fields
-  result: UInt32,
-  player1Score: UInt32,
-  player2Score: UInt32,
-  bestOf: UInt32,
+  player2Choice: Field, // field
+  result: Field,
 }) {}
 
 export const rpsProgram = Experimental.ZkProgram({
   publicInput: rpsState,
 
   methods: {
-    initGame: {
-      privateInputs: [],
-      method(publicInput: rpsState) {
-        publicInput.player1Choice.assertEquals(Field(0));
-        publicInput.player2Choice.assertEquals(Field(0));
-
-        publicInput.result.assertEquals(UInt32.from(10));
-        publicInput.player1Score.assertEquals(UInt32.from(0));
-        publicInput.player2Score.assertEquals(UInt32.from(0));
-        publicInput.bestOf.assertEquals(UInt32.from(3));
-      },
-    },
-
     p1Move: {
-      privateInputs: [Field, Field, SelfProof],
-      method(
-        publicInput: rpsState,
-        move: Field,
-        secret: Field,
-        prevProof: SelfProof<rpsState>
-      ) {
-        prevProof.verify();
-        move.assertGte(Field(0));
-        move.assertLte(Field(2));
-        publicInput.player2Choice.assertEquals(
-          prevProof.publicInput.player2Choice
-        );
+      privateInputs: [Field, Field],
+      method(publicInput: rpsState, move: Field, secret: Field) {
+        // assert valid start state:
+        publicInput.player2Choice.assertEquals(Field(-1));
+        publicInput.result.assertEquals(Field(-1));
 
-        // p1 makes a valid move
+        // assert move choice is within range:
+        move.assertGte(Field(1));
+        move.assertLte(Field(3));
+
+        // p1 makes a valid move:
         publicInput.player1Choice.assertEquals(Poseidon.hash([move, secret]));
       },
     },
 
     p2Move: {
-      privateInputs: [Field, Field, SelfProof],
+      privateInputs: [Field, SelfProof],
       method(
         publicInput: rpsState,
         move: Field,
-        secret: Field,
         prevProof: SelfProof<rpsState>
       ) {
+        // verify p1 move is valid:
         prevProof.verify();
-        move.assertGte(Field(0));
-        move.assertLte(Field(2));
         publicInput.player1Choice.assertEquals(
           prevProof.publicInput.player1Choice
         );
 
+        // assert move choice is within range:
+        move.assertGte(Field(1));
+        move.assertLte(Field(3));
+
         // p2 makes a valid move
-        publicInput.player2Choice.assertEquals(Poseidon.hash([move, secret]));
+        publicInput.player2Choice.assertEquals(move);
+      },
+    },
+
+    checkWinner: {
+      privateInputs: [Field, Field, Field, SelfProof],
+      method(
+        publicInput: rpsState,
+        confirmP1Move: Field,
+        confirmP2Move: Field,
+        secret: Field,
+        prevProof: SelfProof<rpsState>
+      ) {
+        prevProof.verify();
+        // Assert p1 move has NOT been altered:
+
+        // 1. Assert current p1 choice has not changed from last proof
+        publicInput.player1Choice.assertEquals(
+          prevProof.publicInput.player1Choice
+        );
+        // 2. confirm Field move provided corresponds to current p1 choice
+        let confirmed = Poseidon.hash([confirmP1Move, secret]);
+        publicInput.player1Choice.assertEquals(confirmed);
+
+        // Assert p1 is NOT altering the move p2 provided:
+        confirmP2Move.assertEquals(prevProof.publicInput.player2Choice);
+
+        // TODO: compare publicInput.player2choice with confirmMove & secret
+        let theWinner = Circuit.if(
+          // if p1 = rock and p2 = paper,
+          confirmP1Move.equals(Field(1)).and(confirmP2Move.equals(Field(2))),
+          // p2 wins
+          Field(2),
+          // else assume p2 = scissors and p1 wins
+          Field(1)
+        );
+        publicInput.result.assertEquals(theWinner);
       },
     },
   },
--- contracts/src/run-rpsProgram.ts
+++ contracts/src/run-rpsProgram.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { Field, Poseidon, UInt32, isReady, shutdown } from 'snarkyjs';
+import { Field, Poseidon, isReady, shutdown } from 'snarkyjs';
 import { tic, toc } from './tictoc.js';
 import { rpsProgram, rpsState } from './rpsProgram.js';
 
@@ -8,101 +8,76 @@ async function main() {
   await isReady;
   toc();
 
+  // Compile
   tic('Compiling');
   await rpsProgram.compile();
   toc();
 
-  let initState = new rpsState({
-    player1Choice: Field(0), // secret fields
-    player2Choice: Field(0), // secret fields
+  // --------------------------------------
+  // Test case: p1 always chooses rock (1):
+  // p2 chooses paper (2)
 
-    result: UInt32.from(10),
-    player1Score: UInt32.from(0),
-    player2Score: UInt32.from(0),
-    bestOf: UInt32.from(3),
+  // 1. p1 chooses rock:
+
+  // private inputs
+  const myP1Move = Field(1);
+  const myP1Secret = Field(24);
+  const p1choice = Poseidon.hash([myP1Move, myP1Secret]); // rock with a secret of 24
+
+  // public state
+  let p1State = new rpsState({
+    player1Choice: p1choice,
+    player2Choice: Field(-1),
+    result: Field(-1),
   });
 
-  // Initialize Game State PROOF
-  tic('proof0');
-  const proof0 = await rpsProgram.initGame(initState);
+  // proof
+  tic('PROOF 1: p1 chooses rock');
+  const proof1 = await rpsProgram.p1Move(p1State, myP1Move, myP1Secret);
   toc();
 
-  let p1MovedState = new rpsState({
-    player1Choice: Poseidon.hash([Field(1), Field(24)]),
-    player2Choice: Field(0), // secret fields
+  // 2. p2 chooses paper:
 
-    result: UInt32.from(10),
-    player1Score: UInt32.from(0),
-    player2Score: UInt32.from(0),
-    bestOf: UInt32.from(3),
+  // private inputs
+  const myP2Move = Field(2);
+  const p2choice = myP2Move;
+
+  // public state
+  let p2State = new rpsState({
+    player1Choice: proof1.publicInput.player1Choice,
+    player2Choice: p2choice,
+    result: Field(-1),
   });
 
-  tic('proof P1 made a move');
-  const proofP1 = await rpsProgram.p1Move(
-    p1MovedState,
-    Field(1),
-    Field(24),
-    proof0
+  // proof
+  tic('PROOF 2: p2 chooses paper');
+  const proof2 = await rpsProgram.p2Move(p2State, myP2Move, proof1);
+  toc();
+
+  // 3. p1 checks winner:
+
+  // private inputs
+  const confirmP1 = myP1Move;
+  const confirmP2 = proof2.publicInput.player2Choice;
+  const secret = myP1Secret;
+
+  // public state
+  let confirmState = new rpsState({
+    player1Choice: proof2.publicInput.player1Choice,
+    player2Choice: proof2.publicInput.player2Choice,
+    result: Field(2),
+  });
+
+  // proof
+  tic('PROOF 2: p2 chooses paper');
+  const proof3 = await rpsProgram.checkWinner(
+    confirmState,
+    confirmP1,
+    confirmP2,
+    secret,
+    proof2
   );
   toc();
-
-  let p2MovedState = new rpsState({
-    player1Choice: Poseidon.hash([Field(1), Field(24)]),
-    player2Choice: Poseidon.hash([Field(2), Field(12)]),
-
-    result: UInt32.from(10),
-    player1Score: UInt32.from(0),
-    player2Score: UInt32.from(0),
-    bestOf: UInt32.from(3),
-  });
-
-  tic('proof P2 made a move');
-  const proofP2 = await rpsProgram.p2Move(
-    p2MovedState,
-    Field(2),
-    Field(12),
-    proofP1
-  );
-  toc();
-
-  // tic('proof P1 made a move');
-  // const proofP2 = await rpsProgram.p2Move(p1MovedState, Field(1), Field(24), proof0)
-  // toc();
-
-  // let validGameState = new rpsState({
-  //   player1Choice: Field(-1),
-  //   player2Choice: Field(-1),
-  //   result: Field(-1),
-  //   player1Score: UInt32.from(0),
-  //   player2Score: UInt32.from(0),
-  //   bestOf: UInt32.from(3),
-  // });
-  // let hashInitGameState = hashRpsState(validGameState);
-
-  // let myGameState = new rpsState({
-  //   player1Choice: Field(-1),
-  //   player2Choice: Field(-1),
-  //   result: Field(-1),
-  //   player1Score: UInt32.from(0),
-  //   player2Score: UInt32.from(0),
-  //   bestOf: UInt32.from(3),
-  // });
-  // let hashMyGameState = hashRpsState(myGameState);
-
-  // Initialize Game State PROOF
-  // tic('proof with Hash');
-  // const proof1 = await rpsProgram.checkWithHash(
-  //   hashInitGameState,
-  //   hashMyGameState
-  // );
-  // toc();
-
-  // tic('proof with State');
-  // const proof2 = await rpsProgram.checkWithState(
-  //   hashInitGameState,
-  //   myGameState
-  // );
-  // toc();
 
   await shutdown();
 }
